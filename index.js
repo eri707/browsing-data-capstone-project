8c8f8ff964b1bdb57b54b0e20ca8ace000e76211
eri <eri0.sagittarius@gmail.com>
2020-12-16 10:43:33 -0800

    renamed variables and added comments

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const fs = require('fs');
 fs.readFile('./cars.json', (err, data) => {
     if (err) return console.log('Invalid data');
     let cars = JSON.parse(data);
-    // assign argv from CLI
-    let inPutMakeorType = process.argv[2];
-    if (!inPutMakeorType) return console.log('Please input a make or type of a car')
-    let inputMakeandType = process.argv[3]
-    // shows a make or model or type of a car 
-    cars.forEach(element => {
-        // print model and type of a car  if a user input only a make of car
-        if (element.make.toUpperCase() == inPutMakeorType.toUpperCase() && !inputMakeandType) {
-            console.log(`model: ${element.model}, type: ${element.type}`)
-        // print make and model of a car if a user input only a type of car
-        } else if (element.type.toUpperCase() == inPutMakeorType.toUpperCase()) {
-            console.log(`make: ${element.make}, model: ${element.model}`)
-        // print type of car if a user input both make and type of a car
-        } else if(element.make.toUpperCase() == inPutMakeorType.toUpperCase() && element.type.toUpperCase() == inputMakeandType.toUppercase()) {
-            console.log(`type: ${element.model}`)
-        }
+    // assign argvs from CLI
+    let arg1 = process.argv[2];
+    if (!arg1) return console.log('Please input a make or type of a car')
+    let arg2 = process.argv[3]
+    // filter user inputs
+    let carList = cars.filter(car => {
+             // return car if make matches but type doesn't exist
+        return (car.make.toUpperCase() == arg1.toUpperCase()) && !arg2 ||
+            // return car if make and type match
+            (car.make.toUpperCase() == arg1.toUpperCase()) && (car.type.toUpperCase() == arg2.toUpperCase()) ||
+            // return car if type matches
+            (car.type.toUpperCase() == arg1.toUpperCase());
+    });
+    // print car model 
+    carList.forEach(car => {
+        console.log(`${car.model}`);
     })
 })
